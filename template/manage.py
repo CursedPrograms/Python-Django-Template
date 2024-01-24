78b6531b7044c22339077b5ae362c919f1c5a0f3
Cursed Entertainment <cursedsoft@gmail.com>
2024-01-24 15:05:59 +0200

    init

--- template/manage.py
+++ template/manage.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project_name.settings")
 
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'template.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -13,5 +15,8 @@ if __name__ == "__main__":
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-
     execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
